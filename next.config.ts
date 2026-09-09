4921850b6f1c20c46a2cd58b5a5a158a81dbf661
shayshayvn <shayshay@neovate.asia>
2026-09-09 23:01:39 +0700

    feat: configure turbopack root path in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
+import path from "path";
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: path.resolve("."),
+  },
   async redirects() {
     return [
       {
